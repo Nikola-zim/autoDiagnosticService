7f357c06d5da00f80859ee13e553487560b01aca
NikolaZim <n.zimenkov.2020@gmail.com>
2023-04-30 03:33:10 +0300

    baseInit0: fileUploading

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ type (
 		HTTP `yaml:"http"`
 		Log  `yaml:"logger"`
 		PG   `yaml:"postgres"`
-		RMQ  `yaml:"rabbitmq"`
+		//RMQ  `yaml:"rabbitmq"`
 	}
 
 	// App -.
@@ -35,15 +35,15 @@ type (
 	// PG -.
 	PG struct {
 		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
-		URL     string `env-required:"true"                 env:"PG_URL"`
+		URL     string `env-required:"true" yaml:"pg_url"   env:"PG_URL"`
 	}
 
-	// RMQ -.
-	RMQ struct {
-		ServerExchange string `env-required:"true" yaml:"rpc_server_exchange" env:"RMQ_RPC_SERVER"`
-		ClientExchange string `env-required:"true" yaml:"rpc_client_exchange" env:"RMQ_RPC_CLIENT"`
-		URL            string `env-required:"true"                            env:"RMQ_URL"`
-	}
+	//// RMQ -.
+	//RMQ struct {
+	//	ServerExchange string `env-required:"true" yaml:"rpc_server_exchange" env:"RMQ_RPC_SERVER"`
+	//	ClientExchange string `env-required:"true" yaml:"rpc_client_exchange" env:"RMQ_RPC_CLIENT"`
+	//	URL            string `env-required:"true"                            env:"RMQ_URL"`
+	//}
 )
 
 // NewConfig returns app config.
--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -2,37 +2,21 @@
 package v1
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-
-	// Swagger docs.
-	_ "github.com/evrone/go-clean-template/docs"
 	"github.com/evrone/go-clean-template/internal/usecase"
 	"github.com/evrone/go-clean-template/pkg/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // NewRouter -.
-// Swagger spec:
-// @title       Go Clean Template API
-// @description Using a translation service as an example
-// @version     1.0
-// @host        localhost:8080
-// @BasePath    /v1
 func NewRouter(handler *gin.Engine, l logger.Interface, t usecase.Translation) {
 	// Options
 	handler.Use(gin.Logger())
 	handler.Use(gin.Recovery())
 
-	// Swagger
-	swaggerHandler := ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "DISABLE_SWAGGER_HTTP_HANDLER")
-	handler.GET("/swagger/*any", swaggerHandler)
-
-	// K8s probe
-	handler.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
+	// Static файлы
+	handler.Static("/internal/controller/static/templates/css", "./internal/controller/static/templates/css")
+	handler.LoadHTMLGlob("./internal/controller/static/templates/html/*.html")
 
 	// Prometheus metrics
 	handler.GET("/metrics", gin.WrapH(promhttp.Handler()))
@@ -41,5 +25,9 @@ func NewRouter(handler *gin.Engine, l logger.Interface, t usecase.Translation) {
 	h := handler.Group("/v1")
 	{
 		newTranslationRoutes(h, t, l)
+	}
+	iconRecognition := handler.Group("/iconRecognition")
+	{
+		newIconRecognitionRoutes(iconRecognition, t, l)
 	}
 }
